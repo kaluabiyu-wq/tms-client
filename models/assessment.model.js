5fd65bf662686530b2f75443fc7980f003fe4662
kaluabiyu-wq <kaluabiyu@gmail.com>
2026-06-09 15:57:37 +0300

    Delete models/assessment.model.js

--- models/assessment.model.js
+++
@@ -1,8 +0,0 @@
-export function calculateGrade(item) {
-    switch (item.kind) {
-        case "quiz":
-            return Math.round((item.correctAnswers / item.totalQuestions) * 100);
-        case "lab":
-            return Math.round(item.functionalityScore * 0.7 + item.codeQualityScrore * 0.3);
-    }
-}
